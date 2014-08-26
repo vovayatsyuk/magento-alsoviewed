ae309d32d98efdda021c024132627c9a5052d3fa
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2014-08-26 23:41:33 +0300

    Configuration section added. #3 related

---
+++ code/Model/System/Config/Source/ListMode.php
@@ -0,0 +1,12 @@
+<?php
+
+class Yavva_AlsoViewed_Model_System_Config_Source_ListMode
+{
+    public function toOptionArray()
+    {
+        return array(
+            array('value' => 'grid', 'label' => Mage::helper('catalog')->__('Grid')),
+            array('value' => 'list', 'label' => Mage::helper('catalog')->__('List'))
+        );
+    }
+}
