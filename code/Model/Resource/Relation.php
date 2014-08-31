d9acf0859658e90a20a3d1a3c4c10ba8dadf3bf2
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2014-08-31 22:10:24 +0300

    Update relations method should use the write adapter

--- code/Model/Resource/Relation.php
+++ code/Model/Resource/Relation.php
@@ -7,6 +7,21 @@ class Yavva_AlsoViewed_Model_Resource_Relation extends Mage_Core_Model_Resource_
         $this->_init('alsoviewed/relation', 'relation_id');
     }
 
+    /**
+     * Insert new and update existing relations according to $relationsData
+     *
+     * @param  array   $relationsData   Array of product to product relations:
+     *                                  array(
+     *                                      array(
+     *                                          product_id          => int
+     *                                          related_product_id  => int
+     *                                          weight              => int
+     *                                      )
+     *                                      ...
+     *                                  )
+     * @param  boolean $bidirectional
+     * @return int     Number of affected rows
+     */
     public function updateRelations($relationsData, $bidirectional = true)
     {
         $data = $relationsData;
@@ -15,13 +30,13 @@ class Yavva_AlsoViewed_Model_Resource_Relation extends Mage_Core_Model_Resource_
                 $data[] = array(
                     'product_id'         => $relation['related_product_id'],
                     'related_product_id' => $relation['product_id'],
-                    'weight'             => $relation['weight'],
+                    'weight'             => $relation['weight']
                 );
             }
         }
 
-        $adapter = $this->_getReadAdapter();
-        $adapter->insertOnDuplicate($this->getMainTable(), $data, array(
+        $adapter = $this->_getWriteAdapter();
+        return $adapter->insertOnDuplicate($this->getMainTable(), $data, array(
             'weight' => new Zend_Db_Expr('weight + VALUES(weight)')
         ));
     }
