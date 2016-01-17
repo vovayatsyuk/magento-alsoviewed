bcd27f63acc3a375031470a308da5bda6e8678bf
Vova Yatsyuk <vova.yatsyuk@gmail.com>
2016-01-17 15:03:27 +0200

    Fixed possible error during log processing, if some products where deleted.

--- app/code/community/Yavva/Alsoviewed/Model/Resource/Log.php
+++ app/code/community/Yavva/Alsoviewed/Model/Resource/Log.php
@@ -38,6 +38,8 @@ class Yavva_Alsoviewed_Model_Resource_Log extends Mage_Core_Model_Resource_Db_Ab
      */
     public function getGroupedRelations()
     {
+        $this->cleanOrphans();
+
         $adapter = $this->_getReadAdapter();
         $select  = $adapter->select()
             ->from($this->getMainTable(), array(
@@ -59,5 +61,40 @@ class Yavva_Alsoviewed_Model_Resource_Log extends Mage_Core_Model_Resource_Db_Ab
     public function clean($where = '')
     {
         return $this->_getWriteAdapter()->delete($this->getMainTable(), $where);
+    }
+
+    /**
+     * Remove orphans records from table
+     *
+     * @return int Number of deleted records
+     */
+    public function cleanOrphans()
+    {
+        $tableName = Mage::getModel('core/resource')
+            ->getTableName('catalog_product_entity');
+
+        $adapter = $this->_getReadAdapter();
+        $select  = $adapter->select();
+
+        $select->from($this->getMainTable(), 'entity_id')
+            ->joinLeft(
+                array('e' => $tableName),
+                'product_id = e.entity_id',
+                array()
+            )
+            ->orWhere('e.entity_id IS NULL');
+
+        $select->joinLeft(
+                array('e2' => $tableName),
+                'related_product_id = e2.entity_id',
+                array()
+            )
+            ->orWhere('e2.entity_id IS NULL');
+
+        $ids = $adapter->fetchCol($select);
+        if (!$ids) {
+            return 0;
+        }
+        return $this->clean(array('entity_id IN (?)' => $ids));
     }
 }
